a7fcbcd57ed373cb0e49a34f78e8633e5f6f780e
WishHunter <molkovd@ya.ru>
2021-02-05 00:02:33 +0300

    add all homework

--- Swift_Homework/main.swift
+++ Swift_Homework/main.swift
@@ -12,6 +12,10 @@ protocol Transport: class {
     var brand: String { get }
     var model: String { get }
     var gasolineLevel: UInt { get set }
+    var decommissioned: Bool { get set }
+    var residualValue: Double { get set }
+    
+    func getGasolineLevel() -> UInt
 }
 
 class Car: Transport {
@@ -19,12 +23,22 @@ class Car: Transport {
     var brand: String
     var model: String
     var gasolineLevel: UInt
+    var decommissioned: Bool
+    var residualValue: Double {
+        didSet {
+            if residualValue <= 0 && !decommissioned {
+                decommissioned = true
+            }
+        }
+    }
     
-    init(brand: String, model: String) {
-        type = "Car"
+    init(brand: String, model: String, price: Double) {
+        self.type = "Car"
         self.brand = brand
         self.model = model
         self.gasolineLevel = 100
+        self.decommissioned = false
+        self.residualValue = price
     }
     
     func setGasolineLevel(_ gasolineLevel: UInt) {
@@ -33,6 +47,10 @@ class Car: Transport {
     
     func getGasolineLevel() -> UInt {
         return self.gasolineLevel
+    }
+    
+    func setDecomissioned(_ decommissioned: Bool) {
+        self.decommissioned = decommissioned
     }
 }
 
@@ -41,12 +59,22 @@ class Motorcycle: Transport {
     var brand: String
     var model: String
     var gasolineLevel: UInt
+    var decommissioned: Bool
+    var residualValue: Double {
+        didSet {
+            if residualValue <= 0 && !decommissioned {
+                decommissioned = true
+            }
+        }
+    }
     
-    init(brand: String, model: String) {
-        type = "Motorcycle"
+    init(brand: String, model: String, price: Double) {
+        self.type = "Motorcycle"
         self.brand = brand
         self.model = model
         self.gasolineLevel = 100
+        self.decommissioned = false
+        self.residualValue = price
     }
     
     func setGasolineLevel(_ gasolineLevel: UInt) {
@@ -55,6 +83,10 @@ class Motorcycle: Transport {
     
     func getGasolineLevel() -> UInt {
         return self.gasolineLevel
+    }
+    
+    func setDecomissioned(_ decommissioned: Bool) {
+        self.decommissioned = decommissioned
     }
 }
 
@@ -87,16 +119,21 @@ extension Queue {
         let tmpArray: [T] = queueArray
         return tmpArray.filter(customFilter)
     }
+    
+    mutating func deleteDecommissioned(customDelete: (T) -> Bool) -> [T] {
+        queueArray.removeAll(where: customDelete)
+        return queueArray
+    }
 }
 
-var car1 = Car(brand: "KIA", model: "RIO")
-var car2 = Car(brand: "Mercedes", model: "E200")
-var car3 = Car(brand: "Chevrolet", model: "Cruze")
-var car4 = Car(brand: "MINI", model: "Countryman")
-var moto1 = Motorcycle(brand: "Honda", model: "CBR 600F")
-var moto2 = Motorcycle(brand: "Yamaha", model: "XVS 1100")
-var moto3 = Motorcycle(brand: "BMW", model: "F 800 R")
-var moto4 = Motorcycle(brand: "Suzuki", model: "GSX-R 600")
+var car1 = Car(brand: "KIA", model: "RIO", price: 120000)
+var car2 = Car(brand: "Mercedes", model: "E200", price: 120000)
+var car3 = Car(brand: "Chevrolet", model: "Cruze", price: 120000)
+var car4 = Car(brand: "MINI", model: "Countryman", price: 120000)
+var moto1 = Motorcycle(brand: "Honda", model: "CBR 600F", price: 120000)
+var moto2 = Motorcycle(brand: "Yamaha", model: "XVS 1100", price: 120000)
+var moto3 = Motorcycle(brand: "BMW", model: "F 800 R", price: 120000)
+var moto4 = Motorcycle(brand: "Suzuki", model: "GSX-R 600", price: 120000)
 
 var queueTransports = Queue<Transport>()
 
@@ -115,8 +152,10 @@ car3.setGasolineLevel(5)
 car4.setGasolineLevel(0)
 moto4.setGasolineLevel(8)
 
+moto3.residualValue = 0
+
 func checkEmptyGasoline() {
-    let filterGasolineLevel: (Transport) -> Bool = {return $0.gasolineLevel < 30}
+    let filterGasolineLevel: (Transport) -> Bool = {return $0.getGasolineLevel() < 30}
     let emptyGasoline: [Transport] = queueTransports.filterQueue(customFilter: filterGasolineLevel)
     
     print("These transports need to refueling:")
@@ -126,4 +165,39 @@ func checkEmptyGasoline() {
     }
 }
 
+let customDelete: (Transport) -> Bool = {$0.decommissioned}
+let newQueue = queueTransports.deleteDecommissioned(customDelete: customDelete)
+print(newQueue)
+
 checkEmptyGasoline()
+
+
+
+struct Queue2<T: Transport> {
+    private var queueArray: [T] = []
+    
+    mutating func push(_ elem: T) {
+        queueArray.append(elem)
+    }
+    
+    subscript(indices: UInt ...) -> Double {
+        var totalResidualValue: Double = 0.0
+        
+        for index in indices where index < self.queueArray.count {
+            totalResidualValue += self.queueArray[Int(index)].residualValue
+        }
+        
+        return totalResidualValue
+    }
+}
+
+
+var queueTransports2 = Queue2<Motorcycle>()
+
+queueTransports2.push(moto1)
+queueTransports2.push(moto2)
+queueTransports2.push(moto3)
+queueTransports2.push(moto4)
+
+print("=========================")
+print(queueTransports2[0,1,2,3,4])
